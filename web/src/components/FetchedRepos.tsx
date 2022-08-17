fdcb63a05bbf4adb8e1343ae00d8d2461c37b270
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-08-17 10:49:39 -0400

    change app styling, add each fetched repository clickable and display extra detail, note: find out why message not found

--- web/src/components/FetchedRepos.tsx
+++ web/src/components/FetchedRepos.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FetchedRepos.css';
 
 export default function FetchedRepos({ reposData, loading }: any) {
+  const [repoID, setRepoId] = useState(0);
+
+  const clickHandler = (id: any) => {
+    setRepoId(id);
+  };
+
   // map filtered list of repos
   const mappedRepos = reposData.map((repo: any) => {
     return (
-      <div className="repo" key={repo.id}>
+      <div
+        onClick={() => {
+          clickHandler(repo.id);
+        }}
+        className="repo"
+        key={repo.id}
+      >
         <h1>
           <strong>Title:</strong> {repo.name}
         </h1>
@@ -21,7 +33,19 @@ export default function FetchedRepos({ reposData, loading }: any) {
         <p>
           <strong>Number of Forks:</strong> {repo.forks}
         </p>
-        <p>Created At: {repo.created_at}</p>
+        {repoID === repo.id && (
+          <>
+            <p>
+              <strong>Recent Commit Date: </strong>
+            </p>
+            <p>
+              <strong>Author: </strong>
+            </p>
+            <p>
+              <strong>Message: </strong>
+            </p>
+          </>
+        )}
       </div>
     );
   });
