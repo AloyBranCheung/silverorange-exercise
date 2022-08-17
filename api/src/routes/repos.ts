d955e68e6e57f6fb7a70dbb3b64dab42c0a25d6c
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-08-17 07:44:27 -0400

    add cross-env dependency to start server across platforms

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -6,7 +6,6 @@ repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
-
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
   res.json([]);
 });
